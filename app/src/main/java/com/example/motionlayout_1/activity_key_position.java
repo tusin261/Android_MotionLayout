c0ba14657cc1fb3e48395d8f2103335808b75af9
tusin26 <thanhtu3885585@gmail.com>
2021-09-21 14:04:19 +0700

    add activity keyposition

---
+++ app/src/main/java/com/example/motionlayout_1/activity_key_position.java
@@ -0,0 +1,14 @@
+package com.example.motionlayout_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_key_position extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_key_position);
+    }
+}
